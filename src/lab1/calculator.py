a77a092a0381198fd6bdbe62bda93d30fb1d4ce0
Tart1lya <iiigametv2006@gmail.com>
2024-09-30 20:53:13 +0300

    [feat]: added operations of sum, difference, multiplication, division and ascending to degree

---
+++ src/lab1/calculator.py
@@ -0,0 +1,14 @@
+def make_sum(a, b):
+    return a + b
+
+def make_difference(a, b):
+    return a - b
+
+def make_product(a, b):
+    return a * b
+
+def make_quotient(a, b):
+    return a / b
+
+def make_pow(a, b):
+    return a ** b
